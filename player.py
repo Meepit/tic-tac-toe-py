21d2a3f9c3333c05601f143c3d0e204d0c48a3e2
James Lizamore <jwlizamore@gmail.com>
2018-02-01 23:25:01 +0000

    Added player, initialized with piece attribute for X or O

---
+++ player.py
@@ -0,0 +1,8 @@
+class Player():
+    def __init__(self, piece):
+        self.piece = piece
+
+    def get_piece(self):
+        return self.piece
+
+    
---
+++ tests/test_player.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest import mock
+from unittest.mock import MagicMock
+import mock
+
+from player import Player
+
+class PlayerTest(unittest.TestCase):
+    def setUp(self):
+        self.player = Player('X')
+
+    def test_get_piece(self):
+        self.assertEqual('X', self.player.piece)
