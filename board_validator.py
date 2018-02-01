1fe1bc409428d490bce42d6597da181dc632c027
James Lizamore <jwlizamore@gmail.com>
2018-02-01 12:22:17 +0000

    Fixed import issue in boardvalidatortest, added basic test for non_diagonal_validation

--- board_validator.py
+++ board_validator.py
@@ -1,6 +1,11 @@
 class BoardValidator():
-    def __init__(sef, board_state):
+    # Validates 2d array string boards
+    def __init__(self, board_state):
         self.board_state = board_state
 
     def transpose_board(self):
         return [[self.board_state[j][i] for j in range(3)] for i in range(3)]
+
+    def validate_non_diagonals(self):
+        # if True then game over
+        return True in ["".join(i) == "XXX" or "".join(i) == "OOO" for i in self.board_state + self.transpose_board()]
--- tests/test_board_validator.py
+++ tests/test_board_validator.py
@@ -1,13 +1,19 @@
 import unittest
 from unittest import mock
 from unittest.mock import MagicMock
+from board_validator import BoardValidator
 
 
 class TestBoardValidator(unittest.TestCase):
     def setUp(self):
-        self.board_class = mock.Mock()
-        self.board_validator = BoardValidator()
+        self.board_state = mock.Mock()
+        self.board_validator = BoardValidator(self.board_state)
 
-    def validates_board_transpose(self):
-        self.board_class.board_state = [[0,1,2], [0,1,2], [0,1,2]]
-        self.assertEqual(self.board_class.transpose_board(), [[0,0,0], [1,1,1], [2,2,2]])
+    def test_validates_board_transpose(self):
+        self.board_validator.board_state = [[0,1,2], [0,1,2], [0,1,2]]
+        self.assertEqual(self.board_validator.transpose_board(), [[0,0,0], [1,1,1], [2,2,2]])
+
+    def test_validate_non_diagonal(self):
+        board_state = [['X', 'X', 'X'],['X', 'O', 'X'],['O', 'X', 'X']]
+        self.board_validator.board_state = board_state
+        self.assertTrue(self.board_validator.validate_non_diagonals())
