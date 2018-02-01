9c0a3904f87ebbe0ee7712ca42b854ecbc0ff7ad
James Lizamore <jwlizamore@gmail.com>
2018-02-01 12:27:16 +0000

    Added more non diagonal validation tests

--- tests/test_board_validator.py
+++ tests/test_board_validator.py
@@ -13,7 +13,17 @@ class TestBoardValidator(unittest.TestCase):
         self.board_validator.board_state = [[0,1,2], [0,1,2], [0,1,2]]
         self.assertEqual(self.board_validator.transpose_board(), [[0,0,0], [1,1,1], [2,2,2]])
 
-    def test_validate_non_diagonal(self):
+    def test_validate_horizontal_X_winning_position(self):
         board_state = [['X', 'X', 'X'],['X', 'O', 'X'],['O', 'X', 'X']]
+        self.board_validator.board_state = board_state
+        self.assertTrue(self.board_validator.validate_non_diagonals())
+
+    def test_validate_vertical_X_winning_position(self):
+        board_state = [['X', 'O', 'X'], ['X', 'X', 'O'], ['X', 'O', 'X']]
+        self.board_validator.board_state = board_state
+        self.assertTrue(self.board_validator.validate_non_diagonals())
+
+    def test_validate_horizontal_O_winning_position(self):
+        board_state = [['O', 'O', 'O'], ['X', 'X', 'O'], ['O', 'O', 'X']]
         self.board_validator.board_state = board_state
         self.assertTrue(self.board_validator.validate_non_diagonals())
